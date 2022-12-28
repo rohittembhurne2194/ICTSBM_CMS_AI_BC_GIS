5d005be2fb95eea122b4f00109ed58c9af975888
shubham1994s <shubhamkawle41@gamil.com>
2022-12-28 19:09:10 +0530

    comit by shubham

--- SwachBharat.CMS.Dal.DataContexts/ChildSwachBharatAbhiyan.Designer.cs
+++ SwachBharat.CMS.Dal.DataContexts/ChildSwachBharatAbhiyan.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\LiveServer\LiveServer\LocalServer\All Ulb\ICTSBMCMS\ICTSBMCMS\SwachBharat.CMS.Dal.DataContexts\ChildSwachBharatAbhiyan.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ICTSBMCMS-AI-BC-GIS\ICTSBMCMS\SwachBharat.CMS.Dal.DataContexts\ChildSwachBharatAbhiyan.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
